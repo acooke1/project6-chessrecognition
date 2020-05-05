b1fb1688a4093b6209c96a515603ed529c2ca426
daniellerozenblit <daniellerozenblit@gmail.com>
2020-05-05 14:41:41 -0400

    update model

--- hyperparameters.py
+++ hyperparameters.py
@@ -21,17 +21,16 @@ preprocess_sample_size = 400
 # num_epochs is the number of epochs. If you experiment with more
 # complex networks you might need to increase this. Likewise if you add
 # regularization that slows training.
-num_epochs = 30
+num_epochs = 40
 
 # batch_size defines the number of training examples per batch:
 # You don't need to modify this.
-batch_size = 100
+batch_size = 10
 
 # learning_rate is a critical parameter that can dramatically affect
 # whether training succeeds or fails. For most of the experiments in this
 # project the default learning rate is safe.
-learning_rate = 1e-3
-#learning_rate = 1e-4
+learning_rate = 1e-4
 
 # Momentum on the gradient (if you use a momentum-based optimizer)
 momentum = 0.01
