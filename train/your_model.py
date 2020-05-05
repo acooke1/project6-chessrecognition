b61b09576316c12521ed4d75a3dc1488d296f106
daniellerozenblit <daniellerozenblit@gmail.com>
2020-05-05 18:39:01 -0400

    simple model test

--- train/your_model.py
+++ train/your_model.py
@@ -14,7 +14,6 @@ class YourModel(tf.keras.Model):
             learning_rate=hp.learning_rate,
             momentum=hp.momentum)
 
-        
         self.architecture = [
         # Block 1
         Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv1"),
@@ -27,17 +26,36 @@ class YourModel(tf.keras.Model):
         # Block 3
         Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv1"),
         Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv2"),
-        Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv3"),
         MaxPool2D(2, name="block3_pool"),
-        # Block 4
-        Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv1"),
-        Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv2"),
-        Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv3"),
-        MaxPool2D(2, name="block4_pool"),
+    
         Dropout(0.3),
         Flatten(),
         Dense(13,activation='softmax')
         ]
+        
+        # self.architecture = [
+        # # Block 1
+        # Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv1"),
+        # Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv2"),
+        # MaxPool2D(2, name="block1_pool"),
+        # # Block 2
+        # Conv2D(128, 3, 1, padding="same", activation="relu", name="block2_conv1"),
+        # Conv2D(128, 3, 1, padding="same", activation="relu", name="block2_conv2"),
+        # MaxPool2D(2, name="block2_pool"),
+        # # Block 3
+        # Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv1"),
+        # Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv2"),
+        # Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv3"),
+        # MaxPool2D(2, name="block3_pool"),
+        # # Block 4
+        # Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv1"),
+        # Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv2"),
+        # Conv2D(512, 3, 1, padding="same", activation="relu", name="block4_conv3"),
+        # MaxPool2D(2, name="block4_pool"),
+        # Dropout(0.3),
+        # Flatten(),
+        # Dense(13,activation='softmax')
+        # ]
 
     def call(self, img):
         """ Passes input image through the network. """
