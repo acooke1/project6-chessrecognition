12e55c2187f31713c4158d94ec8436f9ba779561
daniellerozenblit <daniellerozenblit@gmail.com>
2020-05-05 19:13:36 -0400

    new model

--- train/your_model.py
+++ train/your_model.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import hyperparameters as hp
-from tensorflow.keras.layers import \
-        Conv2D, MaxPool2D, Dropout, Flatten, Dense
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, Dense
 
 class YourModel(tf.keras.Model):
     """ Your own neural network model. """
@@ -16,17 +15,21 @@ class YourModel(tf.keras.Model):
 
         self.architecture = [
         # Block 1
-        Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv1"),
-        Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv2"),
+        Conv2D(32, 3, 1, padding="same", activation="relu", name="block1_conv1"),
+        Conv2D(32, 3, 1, padding="same", activation="relu", name="block1_conv2"),
         MaxPool2D(2, name="block1_pool"),
         # Block 2
-        Conv2D(128, 3, 1, padding="same", activation="relu", name="block2_conv1"),
-        Conv2D(128, 3, 1, padding="same", activation="relu", name="block2_conv2"),
+        Conv2D(64, 3, 1, padding="same", activation="relu", name="block2_conv1"),
+        Conv2D(64, 3, 1, padding="same", activation="relu", name="block2_conv2"),
         MaxPool2D(2, name="block2_pool"),
         # Block 3
-        Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv1"),
-        Conv2D(256, 3, 1, padding="same", activation="relu", name="block3_conv2"),
+        Conv2D(128, 3, 1, padding="same", activation="relu", name="block3_conv1"),
+        Conv2D(128, 3, 1, padding="same", activation="relu", name="block3_conv2"),
         MaxPool2D(2, name="block3_pool"),
+        # Block 4
+        Conv2D(256, 3, 1, padding="same", activation="relu", name="block4_conv2"),
+        Conv2D(256, 3, 1, padding="same", activation="relu", name="block4_conv3"),
+        MaxPool2D(2, name="block4_pool"),
     
         Dropout(0.3),
         Flatten(),
