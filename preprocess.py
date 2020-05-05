8ea4e381dadde96414b864cebf83301004d178ea
daniellerozenblit <daniellerozenblit@gmail.com>
2020-05-05 17:12:17 -0400

    four blocks

--- preprocess.py
+++ preprocess.py
@@ -139,8 +139,8 @@ class Datasets():
         if augment:
 
             data_gen = tf.keras.preprocessing.image.ImageDataGenerator(
-                preprocessing_function=self.preprocess_fn, rotation_range=10, 
-                width_shift_range=0.1, height_shift_range=0.1)
+                preprocessing_function=self.preprocess_fn, rotation_range=20, 
+                width_shift_range=0.2, height_shift_range=0.2)
 
         else:
             # Don't modify this
