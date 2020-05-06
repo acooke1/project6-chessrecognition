5eef2eeb0e2d0401585d429096d097f55f643679
daniellerozenblit <daniellerozenblit@gmail.com>
2020-05-06 00:09:16 -0400

    try vgg

--- train/run.py
+++ train/run.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import tensorflow as tf
+from vgg_model import VGGModel
 from your_model import YourModel
 import hyperparameters as hp
 from preprocess import Datasets
@@ -14,9 +15,20 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description="Let's train some neural nets!")
     parser.add_argument(
+        '--task',
+        required=True,
+        choices=['1', '2'],
+        help='''Which task of the assignment to run -
+        training from scratch (1), or fine tuning VGG-16 (2).''')
+    parser.add_argument(
         '--data',
-        default=os.getcwd() + '/data/',
+        default=os.getcwd() + '/../data/',
         help='Location where the dataset is stored.')
+    parser.add_argument(
+        '--load-vgg',
+        default=os.getcwd() + '/vgg16_imagenet.h5',
+        help='''Path to pre-trained VGG-16 file (only applicable to
+        task 2).''')
     parser.add_argument(
         '--load-checkpoint',
         default=None,
@@ -27,11 +39,18 @@ def parse_args():
         the case of task 2, passing a checkpoint path will disable
         the loading of VGG weights.''')
     parser.add_argument(
+        '--confusion',
+        action='store_true',
+        help='''Log a confusion matrix at the end of each
+        epoch (viewable in Tensorboard). This is turned off
+        by default as it takes a little bit of time to complete.''')
+    parser.add_argument(
         '--evaluate',
         action='store_true',
         help='''Skips training and evaluates on the test set once.
         You can use this to test an already trained model by loading
         its checkpoint.''')
+
     return parser.parse_args()
 
 def train(model, datasets, checkpoint_path):
@@ -51,6 +70,10 @@ def train(model, datasets, checkpoint_path):
             profile_batch=0),
         ImageLabelingLogger(datasets)
     ]
+
+    # Include confusion logger in callbacks if flag set
+    if ARGS.confusion:
+        callback_list.append(ConfusionMatrixLogger(datasets))
 
     # Begin training
     model.fit(
@@ -74,11 +97,22 @@ def test(model, test_data):
 def main():
     """ Main function. """
 
-    datasets = Datasets(ARGS.data, 1)
-    model = YourModel()
-    model(tf.keras.Input(shape=(hp.img_size, hp.img_size, 3)))
-    checkpoint_path = "./your_model_checkpoints/"
-    model.summary()
+    datasets = Datasets(ARGS.data, ARGS.task)
+
+    if ARGS.task == '1':
+        model = YourModel()
+        model(tf.keras.Input(shape=(hp.img_size, hp.img_size, 3)))
+        checkpoint_path = "./your_model_checkpoints/"
+        model.summary()
+    else:
+        model = VGGModel()
+        checkpoint_path = "./vgg_model_checkpoints/"
+        model(tf.keras.Input(shape=(224, 224, 3)))
+        model.summary()
+
+        # Don't load pretrained vgg if loading checkpoint
+        if ARGS.load_checkpoint is None:
+            model.load_weights(ARGS.load_vgg, by_name=True)
 
     if ARGS.load_checkpoint is not None:
         model.load_weights(ARGS.load_checkpoint)
@@ -96,9 +130,6 @@ def main():
         test(model, datasets.test_data)
     else:
         train(model, datasets, checkpoint_path)
-
-    # Save Model
-    model.save('saved_model') 
 
 # Make arguments global
 ARGS = parse_args()
