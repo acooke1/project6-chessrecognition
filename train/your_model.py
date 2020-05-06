3890c0e09c64d0f882a1359c61b724b5c3562256
daniellerozenblit <daniellerozenblit@gmail.com>
2020-05-05 21:39:15 -0400

    project 4

--- train/your_model.py
+++ train/your_model.py
@@ -13,25 +13,43 @@ class YourModel(tf.keras.Model):
             learning_rate=hp.learning_rate,
             momentum=hp.momentum)
 
+        # self.architecture = [
+        # # Block 1
+        # Conv2D(16, 3, 1, padding="same", activation="relu", name="block1_conv1"),
+        # Conv2D(16, 3, 1, padding="same", activation="relu", name="block1_conv2"),
+        # MaxPool2D(2,2, name="block1_pool"),
+        # # Block 2
+        # Conv2D(32, 3, 1, padding="same", activation="relu", name="block2_conv1"),
+        # Conv2D(32, 3, 1, padding="same", activation="relu", name="block2_conv2"),
+        # MaxPool2D(2,2, name="block2_pool"),
+        # # Block 3
+        # Conv2D(64, 3, 1, padding="same", activation="relu", name="block3_conv1"),
+        # Conv2D(64, 3, 1, padding="same", activation="relu", name="block3_conv2"),
+        # MaxPool2D(2,2, name="block3_pool"),
+        # # # Block 4
+        # # Conv2D(64, 3, 1, padding="same", activation="relu", name="block4_conv1"),
+        # # Conv2D(64, 3, 1, padding="same", activation="relu", name="block4_conv2"),
+        # Flatten(),
+        # Dropout(0.5),
+        # Dense(13,activation='softmax')
+        # ]
+
         self.architecture = [
-        # Block 1
-        Conv2D(16, 3, 1, padding="same", activation="relu", name="block1_conv1"),
-        Conv2D(16, 3, 1, padding="same", activation="relu", name="block1_conv2"),
-        MaxPool2D(2, name="block1_pool"),
-        # Block 2
-        Conv2D(32, 3, 1, padding="same", activation="relu", name="block2_conv1"),
-        Conv2D(32, 3, 1, padding="same", activation="relu", name="block2_conv2"),
-        MaxPool2D(2, name="block2_pool"),
-        # Block 3
-        Conv2D(64, 3, 1, padding="same", activation="relu", name="block3_conv1"),
-        Conv2D(64, 3, 1, padding="same", activation="relu", name="block3_conv2"),
-        MaxPool2D(2, name="block3_pool"),
-        Dropout(0.5),
-        # Block 4
-        Conv2D(64, 3, 1, padding="same", activation="relu", name="block4_conv1"),
-        Conv2D(64, 3, 1, padding="same", activation="relu", name="block4_conv2"),
-        Flatten(),
-        Dense(13,activation='softmax')
+            Conv2D(32,3,strides=1,activation='relu'),
+            Conv2D(32,3,strides=1,activation='relu'),
+            MaxPool2D(pool_size=2, strides=2),
+
+            Conv2D(64,3,strides=1,activation='relu'),
+            Conv2D(64,3,strides=1,activation='relu'),
+            MaxPool2D(pool_size=2,strides=2),
+
+            Conv2D(128,3,strides=1,activation='relu'),
+            Conv2D(128,3,strides=1,activation='relu'),
+            MaxPool2D(pool_size=2,strides=2),
+            
+            Dropout(0.5),
+            Flatten(),
+            Dense(13,activation='softmax')
         ]
         
         # self.architecture = [
